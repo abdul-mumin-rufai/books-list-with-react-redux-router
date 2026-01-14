85517c2cb8d4410242984b9672614710b0a10c65
abdul-mumin-rufai <rufai.abdulmumin@hotmail.com>
2026-01-14 23:11:39 +0000

    added the firebase configuration file.

---
+++ firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyD0czbjXTzTOdn50z-qcKZiey2i40Snw8w",
+    authDomain: "rufai-projects.firebaseapp.com",
+    projectId: "rufai-projects",
+    storageBucket: "rufai-projects.firebasestorage.app",
+    messagingSenderId: "853269955622",
+    appId: "1:853269955622:web:ee0eb4caafd3887b4552e7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
